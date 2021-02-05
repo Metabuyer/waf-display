7a0af9bf12d0c7d0b8b980cf152afee552a05320
Ahmed Al-Salali <a.s.alsalali@gmail.com>
2021-02-05 09:10:14 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,57 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'waf-display';
+  parsingError = false;
+  data: [];
+
+
+  file: any;
+  fileChanged(e) {
+    this.file = e.target.files[0];
+  }
+
+  setData(v): void {
+    if (Array.isArray(v)) {
+      //@ts-ignore
+      this.data = v;
+    }
+    else {
+      this.data = [];
+      //@ts-ignore
+      this.data.push(v);
+    }
+  }
+
+  async changeListener($event): Promise<void> {
+    let t = await this.readThis($event.target);
+    this.setData(t);
+  }
+
+  readThis(inputValue: any) {
+    let file: File = inputValue.files[0];
+    return new Promise((resolve, reject) => {
+      let myReader: FileReader = new FileReader();
+      let setData = this.setData;
+
+      let y = myReader.onloadend = function (e) {
+
+        let jsonFile;
+        try {
+          jsonFile = JSON.parse(myReader.result.toString());
+        } catch (e) {
+          alert('the file cant be parsed');
+          reject('the file cant be parsed');
+        }
+        resolve(jsonFile);
+        // console.log(jsonIntext);
+      }
+
+      myReader.readAsText(file);
+    });
+  }
+
+
+  setParsingError(v): any {
+    this.parsingError = v;
+  }
 }
